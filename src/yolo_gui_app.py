ed8fa73bb061bee7bb3bf06525e79d43640f6d68
Filippo Notari <notari.filippo@outlook.it>
2025-10-11 18:02:22 +0200

    fix detect function and add custom model

--- src/yolo_gui_app.py
+++ src/yolo_gui_app.py
@@ -46,25 +46,25 @@ class YOLOCameraApp:
         self.cap.set(4, 480)
 
     # --- Detection oggetti ---
-def detect_objects(self, frame):
-    detected = set()
-    results_det = self.model_knife(frame, verbose=False)
-    for result in results_det:
-        class_names = result.names
-        for box in result.boxes:
-            if box.conf[0] > 0.4:
-                x1, y1, x2, y2 = map(int, box.xyxy[0])
-                cls = int(box.cls[0])
-                class_name = class_names[cls]
-                conf = float(box.conf[0])
-                detected.add(f"{class_name} {conf:.2f}")
+    def detect_objects(self, frame):
+        detected = set()
+        results_det = self.model_knife(frame, verbose=False)
+        for result in results_det:
+            class_names = result.names
+            for box in result.boxes:
+                if box.conf[0] > 0.4:
+                    x1, y1, x2, y2 = map(int, box.xyxy[0])
+                    cls = int(box.cls[0])
+                    class_name = class_names[cls]
+                    conf = float(box.conf[0])
+                    detected.add(f"{class_name} {conf:.2f}")
 
-                colour = get_colours(cls)
-                cv2.rectangle(frame, (x1, y1), (x2, y2), colour, 2)
-                cv2.putText(frame, f"{class_name} {conf:.2f}",
-                            (x1, max(y1 - 10, 20)), cv2.FONT_HERSHEY_SIMPLEX,
-                            0.6, colour, 2)
-    return detected
+                    colour = get_colours(cls)
+                    cv2.rectangle(frame, (x1, y1), (x2, y2), colour, 2)
+                    cv2.putText(frame, f"{class_name} {conf:.2f}",
+                                (x1, max(y1 - 10, 20)), cv2.FONT_HERSHEY_SIMPLEX,
+                                0.6, colour, 2)
+        return detected
 
     # --- Pose ---
     def detect_pose(self, frame):
@@ -91,6 +91,7 @@ def detect_objects(self, frame):
                                     cv2.FONT_HERSHEY_SIMPLEX, 0.6, colour, 2)
                     detected_persons.add(f"Persona {idx}")
         return frame, detected_persons
+
 
     # --- Aggiornamento frame ---
     def update_frame(self):
--- src/yolo_run.py
+++ src/yolo_run.py
@@ -1,4 +1,4 @@
 from yolo_gui_app import YOLOCameraApp
 
-app = YOLOCameraApp("yolo11n.pt", "yolo11n-pose.pt") #(object detect, pose detect) Change object with knife dectetion
+app = YOLOCameraApp("runs/detect/train/weights/`best`.pt", "yolo11n-pose.pt") #(object detect, pose detect) yolo11n.pt for object 
 app.run()
