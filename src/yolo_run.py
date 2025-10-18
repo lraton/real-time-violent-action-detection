ea8a5bb5d45bc3b075dec0da856a4006dca9ce90
Filippo Notari <notari.filippo@outlook.it>
2025-10-18 19:00:01 +0200

    Move knife-1 dataset to models/knife/knife-1 directory
    Relocated all files related to the knife-1 dataset, including images, YAML, and documentation, from src/knife-1 to src/models/knife/knife-1 to improve project organization and maintain consistency in dataset storage.

--- src/yolo_run.py
+++ src/yolo_run.py
@@ -1,4 +1,4 @@
 from yolo_gui_app import YOLOCameraApp
 
-app = YOLOCameraApp("runs/detect/train3/weights/best.pt", "yolo11n-pose.pt") #(object detect, pose detect) yolo11n.pt for object 
+app = YOLOCameraApp("models/knife/runs/detect/train3/weights/best.pt", "models/yolo11n-pose.pt") #(object detect, pose detect) yolo11n.pt for object 
 app.run()
