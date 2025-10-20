908311c71cf302f6678157cf38285404679328bf
Iraton <notari.filippo@outlook.it>
2025-10-20 14:42:58 +0200

    Update read_npy.py

--- src/read_npy.py
+++ src/read_npy.py
@@ -1,6 +1,6 @@
 import numpy as np
 print("Loading saved data...")
-loaded = np.load("models/violentvideo/video1_person2.npz")
+loaded = np.load("models/violentvideo/video1_person68.npz")
 data = loaded["data"]
 label = loaded["label"]
 print("Data shape:", data.shape)
